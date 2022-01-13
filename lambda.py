f4c553d0267c4ea37a4975c1560deffbacc4d4b4
Brandon Arreguin <arreguin.tech@gmail.com>
2022-01-13 11:08:15 -0600

    Principal lambda example

---
+++ lambda.py
@@ -0,0 +1,10 @@
+import math
+
+square = lambda x: math.sqrt(x)
+
+num = int(input('Get a square root: '))
+print(f'Result = {square(num)}')
+
+
+# square = lambda x: math.sqrt(x)
+# print(f'Result = {square(25)}')
