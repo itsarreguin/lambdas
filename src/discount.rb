a2ce0bc945d754b5c78ad4af498c0a88cbded7b6
Brandon Arreguin <arreguin.tech@gmail.com>
2022-01-29 17:02:46 -0600

    Ruby discount

--- src/discount.rb
+++ src/discount.rb
@@ -1,1 +1,12 @@
 # Lambda use in a simple Ruby code
+
+
+calc = lambda { |pricing, discount| pricing * (100 - discount) / 100 }
+
+puts "Enter the product price: "
+price = gets.chomp.to_i
+
+puts "Enter your discount %: "
+percent = gets.chomp.to_i
+
+puts "Total price to pay $#{calc.call(price, percent)}"
